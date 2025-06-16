4ebccb4ae9c1e63272cc5c8ce83980a8344df540
Y W <yynnw86@gmail.com>
2025-06-16 12:02:03 -0400

    finish functionality of app.js

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ const player2_score = document.querySelector('#player2');
 
 let player1 = 0;
 let player2 = 0;
+let highScore = 5;
+let gameOver = false;
 const player1point = document.querySelector('#player1point');
 const player2point = document.querySelector('#player2point');
 const reset = document.querySelector('#reset');
 
-player1point.addEventListener('click', () => {player1++; player1_score.innerText = player1});
-player2point.addEventListener('click', () => {player2++; player2_score.innerText = player2});
-reset.addEventListener('click', () => {player1_score.innerText = 0; player2_score.innerText = 0; player1 = 0; player2 = 0})
+player1point.addEventListener('click', () => {
+    if (!gameOver) {
+        player1++; 
+        player1_score.innerText = player1;
+        if (player1 === highScore) {
+            gameOver = true;
+        }
+    }
+    
+});
 
-function checkForWinner() {
-    if 
-}
+player2point.addEventListener('click', () => {
+    if (!gameOver) {
+        player2++; 
+        player2_score.innerText = player2;
+        if (player2 === highScore) {
+            gameOver = true;
+        }
+    }
+});
+
+reset.addEventListener('click', () => {
+    player1_score.innerText = 0; 
+    player2_score.innerText = 0; 
+    player1 = 0; 
+    player2 = 0;
+    gameOver = false;
+})
+
+
